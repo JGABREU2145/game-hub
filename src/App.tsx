6498d10d8b293030491693c9c4f8c35e81947e4b
Justin Abreu <justinabreu92@gmail.com>
2024-02-07 12:47:21 -0600

    added logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Grid, GridItem, Show } from "@chakra-ui/react";
 import { useState } from "react";
+import NavBar from "./components/NavBar";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -11,8 +12,8 @@ function App() {
         lg: `"nav nav" "aside main"`,
       }}
     >
-      <GridItem area="nav" bg="coral">
-        Nav
+      <GridItem area="nav">
+        <NavBar />
       </GridItem>
       <Show above="lg">
         <GridItem area="aside" bg="gold">
